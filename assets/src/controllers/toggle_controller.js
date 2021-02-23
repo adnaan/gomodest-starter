e1120d212cdbd5d6f82a0b4a1c4a5f5f197f5807
Adnaan Badr <adnaan.badr@taxfix.de>
2021-02-24 00:05:02 +0100

    simpler toggling of views

---
+++ assets/src/controllers/toggle_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = [ "toggled" ]
+    static values = {
+        toggleClass: String
+    }
+
+    it(e){
+        e.preventDefault()
+        this.toggledTargets.forEach(item => {
+            console.log("item")
+            item.classList.toggle(this.toggleClassValue)
+        })
+    }
+
+}
